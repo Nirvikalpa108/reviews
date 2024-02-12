a64c554337d7d1f6947587a34188420103c2ce21
Amina <amina.adewusi@gmail.com>
2024-02-12 10:07:14 +0000

    adds parseFile method

--- src/main/scala/com/example/AmazonReviews.scala
+++ src/main/scala/com/example/AmazonReviews.scala
@@ -6,6 +6,7 @@ object AmazonReviews {
 
   def test(): IO[String] = IO.delay("Hello World!")
 
+  def parseFile(rows: List[String]): List[Review] = ???
   def processReviews(reviews: List[Review]): List[ReviewSummary] = ???
   def searchReviews(request: Request, reviews: List[ReviewSummary]): List[Result] = ???
 
